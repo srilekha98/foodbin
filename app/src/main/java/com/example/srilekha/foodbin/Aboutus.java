9c021b70225ceef669ef8dff797bc9d778331450
srilekha98 <sri.agnes98@gmail.com>
2018-04-20 15:23:46 +0530

    Update Aboutus.java

--- app/src/main/java/com/example/srilekha/foodbin/Aboutus.java
+++ app/src/main/java/com/example/srilekha/foodbin/Aboutus.java
@@ -13,6 +13,7 @@ public class Aboutus extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
         
     }
 }
